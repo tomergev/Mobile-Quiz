cea9203e64d75f341d53bba71050de43e7979fc7
Tomer <tomergev21@gmail.com>
2023-08-02 10:41:16 -0700

    There hearts in right hand corner track how many mistakes a player made

--- flag-quiz-2.0/app/flagQuiz.js
+++ flag-quiz-2.0/app/flagQuiz.js
@@ -10,22 +10,20 @@ import {
 } from 'react-native'
 import ProgressBar from 'react-native-progress/Bar'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { AntDesign } from '@expo/vector-icons'
 
 const styles = StyleSheet.create({
   flag: {
     flex: 1,
     height: 200,
   },
-  answer: {
-    alignSelf: 'center',
-    fontSize: 20,
-  }
 })
 
 const FlagQuiz = () => {
   const insets = useSafeAreaInsets()
   const [currentIndex, setCurrentIndex] = useState(0)
-  const [choiceIdSelected, setChoiceIdSelected] = useState(null)
+  const [choiceIdsSelected, setChoiceIdsSelected] = useState([])
+  const [numOfIncorrectSelections, setNumOfIncorrectSelections] = useState(0)
   const params = useLocalSearchParams() || {}
   const quiz = JSON.parse(params.quiz || []) 
 
@@ -37,21 +35,46 @@ const FlagQuiz = () => {
         paddingTop: insets.top,
       }}
     >
+      <View style={{ flexDirection: 'row-reverse' }}>
+        <AntDesign 
+          color='red' 
+          name={numOfIncorrectSelections <= 0 ? 'heart' : 'hearto'} 
+          size={18} 
+          style={{ marginLeft: 2 }}
+        />
+        <AntDesign 
+          color='red' 
+          name={numOfIncorrectSelections <= 1 ? 'heart' : 'hearto'}  
+          size={18} 
+          style={{ marginLeft: 2 }}
+        />
+        <AntDesign 
+          color='red' 
+          name={numOfIncorrectSelections <= 2 ? 'heart' : 'hearto'}  
+          size={18} 
+        />
+      </View>
       <View 
         style={{ 
           flex: 2, 
           justifyContent: 'center',
         }}
       >
-        <ProgressBar 
-          height={10} 
-          progress={(currentIndex / quiz.length)} 
-          width={null} 
-        />
-        <Text style={styles.answer}>
+        <Text 
+          style={{
+            alignSelf: 'center',
+            fontSize: 20,
+          }}
+        >
           {quiz[currentIndex]?.answer?.name}
         </Text>
       </View>
+      <ProgressBar 
+        height={10} 
+        progress={(currentIndex / quiz.length)} 
+        style={{ marginBottom: 2 }}
+        width={null} 
+      />
       <View style={{ flex: 4 }}>
         <FlatList
           data={quiz[currentIndex]?.choices}
@@ -60,15 +83,16 @@ const FlagQuiz = () => {
           numColumns={2}
           renderItem={({ item: choice }) => {
             const choiceStyle = { ...styles.flag }
-            const isChoiceSelected = choice.id === choiceIdSelected
+            const isChoiceSelected = choiceIdsSelected.includes(choice.id)
             if (isChoiceSelected) {
               const isChoiceAnswer = choice.id === quiz[currentIndex]?.answer?.id
               choiceStyle.borderColor = isChoiceAnswer ? 'green' : 'red'
               choiceStyle.borderWidth = 5
             }
             return <Pressable
+              disabled={choiceIdsSelected.includes(choice.id)}
               onPress={() => {
-                setChoiceIdSelected(choice.id)
+                setChoiceIdsSelected([...choiceIdsSelected, choice.id])
 
                 if (choice.id === quiz[currentIndex]?.answer?.id) {
                   setTimeout(() => {
@@ -76,14 +100,16 @@ const FlagQuiz = () => {
                     if (currentIndex <= quiz.length - 1) {
                       setCurrentIndex((currentIndex) => currentIndex + 1)
                     }
-                    setChoiceIdSelected(null)
+                    setChoiceIdsSelected([])
                   }, 400)
+                } else {
+                  setNumOfIncorrectSelections((numOfIncorrectSelections) => numOfIncorrectSelections + 1)
                 }
               }}
               style={choiceStyle}
             >
               <Image             
-                resizeMode="center"
+                resizeMode='center'
                 source={{ uri: choice.flag }} 
                 style={styles.flag}
               />
